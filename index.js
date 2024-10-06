1786b63940d29641f19a0408c65abb0b7c4fa08b
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2024-10-06 11:55:12 +0300

    Adds new file .vscode/settings.json and has modified index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+const num1 = 32
+const num2 = 2
+const multiply = 31 * 2;
+
+
+const random = Math.floor(Math.random() * 1000000) + 1;
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+const numbers = [10, 5, 15, 20, 8];
+const max = Math.max(...numbers);
